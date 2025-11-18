b8e8759f3212608d903b301f4d3de5cdf0a866f1
Luka Verhaert <luka.verhaert@gmail.com>
2025-11-18 23:18:26 +0100

    Initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Zoek in de submap 'BP_HTML_Luka' naar alle .html bestanden
+    "./BP_HTML_Luka/*.html", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
